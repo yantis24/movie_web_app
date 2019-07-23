791957a53128c0155341d436b489517c374b65de
Yanti Suryati <yantisc@gmail.com>
2019-07-23 15:16:08 +0100

    fetching API and building header and search bar

---
+++ src/components/Home/Home.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react';
+import {API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE} from '../../config';
+import HeroImage from '../elements/HeroImage/HeroImage';
+import SearchBar from '../elements/SearchBar/SearchBar';
+import FourColGrid from '../elements/FourColGrid/FourColGrid';
+import MovieThumb from  '../elements/MovieThumb/MovieThumb';
+import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn';
+import Spinner from '../elements/Spinner/Spinner';
+import './Home.css';
+import { end } from 'worker-farm';
+
+class Home extends Component {
+    state = {
+        movies : [],
+        heroImage : null,
+        loading : false,
+        currentPage : 0,
+        totalPage : 0,
+        searchTerm : ''
+    }
+
+
+componentDidMount() {
+    this.setState({loading:true});
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.fetchItems(endpoint);
+}
+
+searchItems = (searchTerm) => {
+    let endpoint = '';
+    this.setState({
+        movies: [],
+        loading: true,
+        searchTerm
+    });
+    
+    if (this.setState.searchTerm === ''){
+        endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    } else {
+        endpoint = `${API_URL}search/movie?api+key=${API_KEY}&language=en-US&query=${searchTerm}`;
+    }
+    this.fetchItems(endpoint);
+}
+
+loadMoreItems = ()  => {
+    let endpoint =  '';
+    this.setState({loading:true});
+
+    if (this.setState.searchTerm === '') {
+        endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+    } else {
+        endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${this.state.searchTerm}`;
+    }
+}
+
+fetchItems = (endpoint) => {
+    fetch(endpoint)
+    .then(result => result.json())
+    .then(result => {
+        console.log(result);
+        this.setState({
+            movies:[ ...this.state.movies, ...result.results],
+            heroImage: this.state.heroImage || result.results[0],
+            loading: false,
+            currentPage: result.page,
+            totalPage: result.total_pages
+        })
+    })
+};
+
+    render () {
+        return(
+            <div className = "rmdb-home">
+            {this.state.heroImage ? 
+              <div>
+                <HeroImage
+                 image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${this.state.heroImage.backdrop_path}`}
+                 title={this.state.heroImage.original_title}
+                 text={this.state.heroImage.overview}
+                />
+                <SearchBar/>
+               </div> : null }
+             <FourColGrid/>
+             <Spinner/>
+             <LoadMoreBtn/>
+            </div>
+        )
+    }
+}
+
+export default Home;
